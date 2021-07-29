e2700f2754e23270797f3abfffc1d3df8baf6f6c
mmhamdy <mohamdy2772@gmail.com>
2021-07-30 01:33:08 +0200

    adding exploratory functions

---
+++ R/functions.R
@@ -0,0 +1,17 @@
+# Basic information about the dataset
+df_info <- function(df){
+  dim <- dim(df)
+  numeric_var <- sum(unlist(lapply(df, is.numeric)))
+  var_types <- unlist(lapply(df, class))
+  cat_var <- sum(var_types %in% c("factor", "character"))
+  missing_any <- any(is.na(df))
+  info <- data.frame(
+    instances = dim[[1]],
+    variables = dim[[2]],
+    numeric = numeric_var,
+    categorical = cat_var,
+    missing_data = missing_any
+  )
+  
+  return(info)
+}
